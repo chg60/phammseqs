f11f4950c5143f31438c1f0c7e1d3c30a39a27e1
Christian Gauthier <chg60@pitt.edu>
2021-09-23 19:55:35 -0400

    made input more straightforward

--- src/phamerate/clustalo.py
+++ src/phamerate/clustalo.py
@@ -1,16 +1,15 @@
 from phamerate.cmd import run_command
 
 
-def clustalo_align(fasta, out_dir):
+def clustalo_align(fasta, align):
     """
     Align a pham using Clustal Omega
 
-    :param fasta:
+    :param fasta: path to a FASTA file to align
     :type fasta: pathlib.Path
-    :param out_dir: path to a FASTA file
-    :type out_dir: pathlib.Path
+    :param align: path where the alignment should go
+    :type align: pathlib.Path
     """
-    align = out_dir.joinpath(f"{fasta.stem}.aln")
     command = f"clustalo -i {fasta} --infmt=fasta -o {align} " \
               f"--outfmt=fasta --output-order=tree-order --threads=1 " \
               f"--seqtype=protein --force"
